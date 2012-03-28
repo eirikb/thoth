c60754101ff54af25e037243da3ff6b049c21fbc
Eirik Brandtzæg <eirikb@eirikb.no>
2012-03-28 14:02:06 +0200

    Ready to handle.. stuff

---
+++ thoth.js
@@ -0,0 +1,41 @@
+var dynamo = db = require('dynamo');
+
+var access = require(__dirname + '/access.json'),
+db = dynamo.createClient(access);
+
+var table = db.get('thoth');
+
+table.fetch(function(err) {
+    if (!err) console.log('Ready');
+});
+
+/*
+table.put({
+    id: "123",
+    name: "eirikb"
+}).save(function() {
+    console.log(arguments);
+});
+*/
+
+/*
+table.fetch(function(err, table) {
+    if (err) return console.error(err)
+
+    console.log("This table has %s items and was created at %s", table.ItemCount, table.CreationDateTime)
+})
+
+table.get({
+    key: '123'
+}).fetch(function() {
+    console.log(arguments)
+});
+*/
+
+function S4() {
+    return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+}
+function guid() {
+    return S4() + S4() + S4() + S4;
+}
+
